64826341f3491f108d77f1b78a9390c6039fc9d9
Miguel Spessatto <miguelmspessatto@gmail.com>
2025-09-24 21:37:44 -0300

    Mudança no número de tentativas

--- joguinho adivinhação.py
+++ joguinho adivinhação.py
@@ -2,10 +2,10 @@ import random
 
 numero_aleatorio = random.randint(1, 100)
 acertou_palpite = False
-tentativas = 5
+tentativas = 6
 
 
-while acertou_palpite == False and tentativas > 0 and tentativas <= 5:
+while acertou_palpite == False and tentativas > 0 and tentativas <= 6:
     palpite = int(input("Insira seu palpite: "))
     if palpite == numero_aleatorio:
         print("Parabéns, você acertou!")
@@ -14,5 +14,6 @@ while acertou_palpite == False and tentativas > 0 and tentativas <= 5:
         tentativas = tentativas - 1
         print(f"Seu palpite é muito alto, você ainda tem {tentativas} tentativas restantes, tente novamente")
     else:
+        tentativas = tentativas - 1
         print(f"Seu palpite é muito baixo, você ainda tem {tentativas} tentativas restantes, tente novamente")
 
